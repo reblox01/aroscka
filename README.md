a793082006e9c5f263fc24bc3bbdc03b97888c76
Sohail Koutari <mirocairo15@gmail.com>
2024-05-20 21:14:17 +0100

    fix some issues in database and api

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# CaseCobra - A Modern Fullstack E-Commerce Shop for Custom Phone Cases
+# Aroscka - A Modern Fullstack E-Commerce Shop for Custom Phone Cases
 
 Built with the Next.js 14 App Router, Postgres, TypeScript, Tailwind & Kinde Auth
 
-![Project Image](https://github.com/joschan21/casecobra/blob/master/public/thumbnail.png)
+![Project Image](https://github.com/reblox01/aroscka/blob/master/public/thumbnail.png)
 
 ## Features
 
@@ -20,21 +20,4 @@ Built with the Next.js 14 App Router, Postgres, TypeScript, Tailwind & Kinde Aut
 - ⌨️ 100% written in TypeScript
 - 🎁 ...much more
 
-## Getting started
 
-To get started with this project, run
-
-```bash
-  git clone https://github.com/joschan21/casecobra.git
-```
-
-and copy the .env.example variables into a separate .env file, fill them out & and that's all you need to get started!
-
-
-## Acknowledgements
-
-- [Kinde](https://link.joshtriedcoding.com/kinde) for making this project possible
-
-## License
-
-[MIT](https://choosealicense.com/licenses/mit/)
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,48 +1,34 @@
-import { createUploadthing, type FileRouter } from 'uploadthing/next'
-import { z } from 'zod'
-import sharp from 'sharp'
-import { db } from '@/db'
-
-const f = createUploadthing()
-
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+ 
+const f = createUploadthing();
+ 
+const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+ 
+// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: '4MB' } })
-    .input(z.object({ configId: z.string().optional() }))
-    .middleware(async ({ input }) => {
-      return { input }
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const user = await auth(req);
+ 
+      // If you throw, the user will not be able to upload
+      if (!user) throw new UploadThingError("Unauthorized");
+ 
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      const { configId } = metadata.input
-
-      const res = await fetch(file.url)
-      const buffer = await res.arrayBuffer()
-
-      const imgMetadata = await sharp(buffer).metadata()
-      const { width, height } = imgMetadata
-
-      if (!configId) {
-        const configuration = await db.configuration.create({
-          data: {
-            imageUrl: file.url,
-            height: height || 500,
-            width: width || 500,
-          },
-        })
-
-        return { configId: configuration.id }
-      } else {
-        const updatedConfiguration = await db.configuration.update({
-          where: {
-            id: configId,
-          },
-          data: {
-            croppedImageUrl: file.url,
-          },
-        })
-
-        return { configId: updatedConfiguration.id }
-      }
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+ 
+      console.log("file url", file.url);
+ 
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
     }),
-} satisfies FileRouter
-
-export type OurFileRouter = typeof ourFileRouter
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof ourFileRouter;
